d845f460e6ae214b4219881c4481f1ae284eb039
Kyohei Tashiro <kyokyo_dc5@yahoo.co.jp>
2021-04-11 20:12:58 +0900

    記述見直し

--- src/App.vue
+++ src/App.vue
@@ -15,20 +15,30 @@ export default {
   
   data(){
     return {
-      addressJP: ""
+      address: "",
+      addressJP:""
     };
   },
 
   methods: {
     async getAddress() {
      const item = await axios.get(
-      `https://apis.postcode-jp.com/api/v4/postcodes?postcode=${this.allAddress}?"apikey=eL4LWS5veBtm3u8Rhe1fs5f0PeEQWDxIz9M3XP2"
+      `https://apis.postcode-jp.com/api/v4/postcodes?postcode=${this.address}?"apikey=eL4LWS5veBtm3u8Rhe1fs5f0PeEQWDxIz9M3XP2"
       `);
 
-      item.allAddress= this.addressJP;
+      // this.addressJP=item.data.allAddress;
+
+      // data.addressJP=jyuusyo.addressJP;
+    
+      const jyuusyo = item.data;
 
 
-      this.addressJP = item.data;
+      this.addressJP = jyuusyo.allAddress;
+
+      
+
+
+
 
 
 
